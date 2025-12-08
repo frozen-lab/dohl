19bc65f5399e7d18ce920b82041739e6f86b0554
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2025-12-08 19:37:42 +0530

    re-init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Caracal
+# Dohl
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,2 @@
-mod scalar;
-
-pub struct Caracal;
-
-impl Caracal {
-    pub fn hash64(buf: &[u8]) -> u64 {
-        scalar::wyhash(buf)
-    }
-}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Dohl;
